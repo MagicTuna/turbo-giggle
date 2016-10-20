bd2391468baf92fe1e6c60134e739c4cf0e7ea9b
MagicTuna <chrisb2202@gmail.com>
2016-10-19 19:58:56 -0500

    Add files via upload

---
+++ whatisyourfavoritething6.py
@@ -0,0 +1,13 @@
+import os
+
+thing = input("What is your favorite thing\n")
+
+favorite = (thing + " is your favorite thing")
+if thing == "not you":
+        print("watch it punk")
+else:
+        print(favorite)
+
+input("")
+
+
